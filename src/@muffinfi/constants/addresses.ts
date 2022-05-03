9439c8ca4df5a1133e8ba9153d7417dd91480150
virtues-milkier <95601955+virtues-milkier@users.noreply.github.com>
2022-05-03 16:42:32 -0400

    Update Lens contract address

--- src/@muffinfi/constants/addresses.ts
+++ src/@muffinfi/constants/addresses.ts
@@ -3,5 +3,5 @@ import { constructSameAddressMap } from 'utils/constructSameAddressMap'
 // FIXME: these are Rinkeby
 export const MUFFIN_HUB_ADDRESSES = constructSameAddressMap('0xA488583a8B2Caecf8e9A576e514E64C8f3B744c8')
 export const MUFFIN_MANAGER_ADDRESSES = constructSameAddressMap('0x9A2eC3b0DDA4edFD434E0e3dFA38F14DB9bD0B18') // prettier-ignore
-export const MUFFIN_LENS_ADDRESSES = constructSameAddressMap('0xd62E0D80B47d69e74Cb4f8dBF71ddedf14628450')
+export const MUFFIN_LENS_ADDRESSES = constructSameAddressMap('0x8079aC9e0aeF93ce2e7025a459Fd05841af787A1')
 export const MUFFIN_QUOTER_ADDRESSES = constructSameAddressMap('0x6c7d35D35D98464B30D8aF1b88DC5F7471A13282')
