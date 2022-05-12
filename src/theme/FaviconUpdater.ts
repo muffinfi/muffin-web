9d6ccc73ffc8f7f1a70d8d3978d14542cb6c4b13
virtues-milkier <95601955+virtues-milkier@users.noreply.github.com>
2022-05-12 17:53:50 -0400

    Use media as the input for favicron handler

--- src/theme/FaviconUpdater.ts
+++ src/theme/FaviconUpdater.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export default function FaviconUpdater() {
   useEffect(() => {
     const media = window.matchMedia('(prefers-color-scheme: dark)')
-    const handler = (e: { matches: boolean }) => {
+    const handler = (e: MediaQueryListEvent | MediaQueryList) => {
       const file = e.matches ? 'favicon-dark.$1' : 'favicon.$1'
       document.querySelectorAll('.js-site-favicon').forEach((elm) => {
         const link = elm instanceof HTMLLinkElement ? elm : undefined
@@ -11,8 +11,8 @@ export default function FaviconUpdater() {
         link.href = link.href.replace(/favicon[^/]*\.(svg|png)$/g, file)
       })
     }
+    handler(media)
     media.addEventListener('change', handler)
-    handler({ matches: media.matches })
     return () => media.removeEventListener('change', handler)
   }, [])
 
