cb9457756e052606866cd4f164f0b925ed849cc2
dkenw <dkenw1618@gmail.com>
2022-07-13 18:57:19 +0800

    Update tier colors

--- src/hooks/useTierColors.ts
+++ src/hooks/useTierColors.ts
@@ -4,5 +4,15 @@ import useTheme from './useTheme'
 
 export default function useTierColors() {
   const theme = useTheme()
-  return useMemo(() => [theme.blue2, theme.yellow1, theme.red1, theme.green1, theme.yellow2, theme.red2], [theme])
+  return useMemo(
+    () => [
+      '#4F79A7', // theme.blue2,
+      theme.yellow1,
+      theme.red1,
+      theme.green1,
+      theme.yellow2,
+      theme.red2,
+    ],
+    [theme]
+  )
 }
