598281a39463fdb19376417480b4d0866917d9ec
dkenw <dkenw1618@gmail.com>
2022-07-14 10:49:50 +0800

    Fix: cannot format negative num to string

--- src/components/LiquidityChart/utils/processData.ts
+++ src/components/LiquidityChart/utils/processData.ts
@@ -168,15 +168,15 @@ export const toFixed = (x: number) => {
   if (Math.abs(x) < 1.0) {
     const e = parseInt(x.toString().split('e-')[1])
     if (e) {
-      x *= Math.pow(10, e - 1)
-      return '0.' + new Array(e).join('0') + x.toString().substring(2)
+      x *= 10 ** (e - 1)
+      return (x < 0 ? '-' : '') + '0.' + '0'.repeat(e - 1) + Math.abs(x).toString().substring(2)
     }
   } else {
     let e = parseInt(x.toString().split('+')[1])
     if (e > 20) {
       e -= 20
-      x /= Math.pow(10, e)
-      return x + new Array(e + 1).join('0')
+      x /= 10 ** e
+      return x + '0'.repeat(e)
     }
   }
   return `${x}`
