37beb7d70518861d33bef71435a4d80192e97a17
dkenw <dkenw1618@gmail.com>
2022-07-28 18:13:42 +0800

    Use contract addresses directly from SDK

--- src/@muffinfi/constants/addresses.ts
+++ src/@muffinfi/constants/addresses.ts
@@ -1,12 +1,1 @@
-import { SupportedChainId } from 'constants/chains'
-
-// FIXME: these are Rinkeby
-export const MUFFIN_HUB_ADDRESSES = {
-  [SupportedChainId.RINKEBY]: '0xb74842648CaA2841E1e20f03d318118343fd4c03',
-}
-export const MUFFIN_MANAGER_ADDRESSES = {
-  [SupportedChainId.RINKEBY]: '0x7069713cDfDA0829111ECcEdcF24C2e3f99Cd467',
-}
-export const MUFFIN_LENS_ADDRESSES = {
-  [SupportedChainId.RINKEBY]: '0x81BBb49E7Fb8C20784A52699008d0bE0Bc8baACB',
-}
+export { MUFFIN_HUB_ADDRESSES, MUFFIN_LENS_ADDRESSES, MUFFIN_MANAGER_ADDRESSES } from '@muffinfi/muffin-sdk'
