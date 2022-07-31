eee5e3284ce64b089ce69d3f8118c129355c9895
dkenw <dkenw1618@gmail.com>
2022-07-31 13:18:10 +0800

    Update faucet link

--- src/@muffinfi/utils/faucet.ts
+++ src/@muffinfi/utils/faucet.ts
@@ -1,6 +1,7 @@
 import { SupportedChainId } from 'constants/chains'
 
-export const FAUCET_URL = 'https://token-faucet-dkenw.vercel.app/'
+export const FAUCET_URL =
+  'https://token-faucet-dkenw.vercel.app/?url=https://raw.githubusercontent.com/dkenw/token-list/master/tokenlist.json'
 
 export const isFaucetSupported = (chainId: number | undefined) => {
   return chainId === SupportedChainId.RINKEBY
