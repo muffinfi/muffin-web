a4c7151ae7a444477904e4dc8de1e5db0809a432
dkenw <dkenw1618@gmail.com>
2022-08-08 18:32:24 +0800

    Add deploy script

---
+++ scripts/pin-to-ipfs.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const dotenv = require('dotenv')
+const { getFilesFromPath, Web3Storage } = require('web3.storage')
+
+dotenv.config({ path: path.join(__dirname, '.env.deployment') })
+
+const token = process.env.WEB3_STORAGE_TOKEN
+if (!token) {
+  throw new Error('no web3 storage token found')
+}
+
+const storage = new Web3Storage({ token, endpoint: new URL('https://api.web3.storage') })
+
+async function main() {
+  const files = await getFilesFromPath(path.join(__dirname, '../build'))
+
+  console.log(`Uploading ${files.length} files`)
+
+  const cid = await storage.put(files, {
+    name: `muffin-site ${new Date().toISOString()}`,
+    wrapWithDirectory: false,
+    onStoredChunk: (chunkSize) => console.log(`stored chunk of ${chunkSize} bytes`),
+  })
+
+  console.log('Content added with CID:', cid)
+}
+
+main().catch(console.error)
